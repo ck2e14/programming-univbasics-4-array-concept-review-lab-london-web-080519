2bc2e783b3a3d23e5f3b81ab995b5b6dc2741ff5
Christopher Kennedy <chris94kennedy@gmail.com>
2019-07-24 18:15:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,11 @@ def find_element_index(array, value_to_find)
     return array.index(value_to_find)
   end
 end
-
+#Here, you define length as the length of the array you'll pass as an argument. Then, you have it iterate 
+#over the array THE NUMBER OF TIMES THAT THERE ARE ELEMENTS IN THE ARRAY (length.times) then you use the block 
+#paramter that is associated with the .times methods (a counter) that we have decided to name |i| so that it 
+#iterates over the next element on the next loop iteration to eventually find the value (if it is present) that
+#it passed to it in the argument for value_to_find.
 
 
 def find_max_value(array)
@@ -13,7 +17,12 @@ def find_max_value(array)
     return array.max 
   end
 end
+#Here, you have again asked it to iterate over the array the number of times that there are elements in the array 
+#along with an incrementor (|i|) so that it checks each element in the array for the subsequent code, which asks
+#for it to check what the max value is. It thus checks all the values and returns the highest one in the array.
 
 def find_min_value(array)
-  # Add your solution here
+  length = array.length
+  length.times do |i|
+    return array.min
 end
